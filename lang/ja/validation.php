52a34de99f250e842516ba970975e013d0e62541
sumitomo yuko <lego001154lbayf@gmail.com>
2026-07-30 10:45:32 +0900

    環境構築完了

---
+++ lang/ja/validation.php
@@ -0,0 +1,10 @@
+<?php
+// lang/ja/validation.php
+
+return [
+    'required' => ':attributeを入力してください。',
+    'string'   => ':attributeは文字列で入力してください。',
+    'max'      => [
+        'string' => ':attributeは:max文字以内で入力してください。',
+    ],
+];
