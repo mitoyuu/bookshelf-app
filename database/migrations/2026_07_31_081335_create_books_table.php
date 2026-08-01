0cfcf8699cb3c806dc34c7dbdf8395109de3978a
sumitomo yuko <lego001154lbayf@gmail.com>
2026-08-01 11:30:00 +0900

    feat: データベースのマイグレーションを作成

---
+++ database/migrations/2026_07_31_081335_create_books_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('books', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')
+                ->constrained()
+                ->cascadeOnDelete();
+            $table->string('title'); // タイトル
+            $table->string('author'); // 著者名
+            $table->char('isbn', 13)->unique(); // ISBN13桁で一意で良いのか？
+            $table->date('published_at'); // 出版日
+            $table->text('description')->nullable(); // 説明（NULL許可）
+            $table->string('image_url', 2048)->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('books');
+    }
+};
