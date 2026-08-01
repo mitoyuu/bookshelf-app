15514786c2f3205f0a2456b8dc1b3f5dcf9bf861
sumitomo yuko <lego001154lbayf@gmail.com>
2026-08-01 15:50:20 +0900

    feat: モデルの作成・リレーションを実装

--- database/migrations/2026_07_31_081538_create_genres_table.php
+++ database/migrations/2026_07_31_081538_create_genres_table.php
@@ -15,7 +15,6 @@ return new class extends Migration
             $table->id();
             $table->string('name')->unique();
             $table->timestamps();
-            $table->unique(['book_id', 'genre_id']);
         });
     }
 
