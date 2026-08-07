ef3db102acb04fe1260448e8e7d81294d085fe58
sumitomo yuko <lego001154lbayf@gmail.com>
2026-08-07 09:33:00 +0900

    fix: book_genreテーブルを修正

---
+++ app/Http/Controllers/GenreController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class GenreController extends Controller
+{
+    // if ($genre->books()->exists()) {
+    // return back()->with('error', '書籍が紐付いているため削除できません。');
+}
+}
