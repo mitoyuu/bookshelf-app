e5df0b77aab5c3bdc7a5862d8cf1038681060952
sumitomo yuko <lego001154lbayf@gmail.com>
2026-08-19 11:19:30 +0900

    feat:ランキング機能を実装

--- app/Http/Controllers/RankingController.php
+++ app/Http/Controllers/RankingController.php
@@ -2,7 +2,20 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Book;
+use Illuminate\View\View;
+
 class RankingController extends Controller
 {
-    //
+    public function index(): View
+    {
+        $rankedBooks = Book::withAvg('reviews', 'rating')
+            ->withCount('reviews')
+            ->has('reviews')
+            ->orderByDesc('reviews_avg_rating')
+            ->take(10)
+            ->get();
+
+        return view('ranking.index', compact('rankedBooks'));
+    }
 }
